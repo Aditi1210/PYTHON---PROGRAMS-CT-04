330900187f63088fe8911694261bf1916539b89e
Aditi1210 <81474485+Aditi1210@users.noreply.github.com>
2021-10-24 10:36:04 +0530

    Create 34.py

---
+++ 34.py
@@ -0,0 +1,17 @@
+def sum(x, y):
+
+    sum = x + y
+
+    if sum in range(1 , 100):
+
+        return sum
+
+    else:
+
+        return product
+
+print(sum(10, 6))
+
+print(sum(10, 2))
+
+print(sum(60 , 75))
