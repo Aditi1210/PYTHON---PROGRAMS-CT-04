9858bc3a27b6ebd4bb8b69065e4f4c67bd068eb7
Aditi1210 <81474485+Aditi1210@users.noreply.github.com>
2021-10-24 10:39:44 +0530

    Create 35.py

---
+++ 35.py
@@ -0,0 +1,15 @@
+def sum_four times(x, y, z):
+
+     sum = x + y + z
+
+  
+
+     if x == y == z:
+
+      sum = sum * 4
+
+     return sum
+
+print(sum_four times(1, 2, 3))
+
+print(sum_four times(3, 3, 3))
