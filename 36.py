1af3ef0a823c8a191427e2e7b767fc803878e5b3
Aditi1210 <81474485+Aditi1210@users.noreply.github.com>
2021-10-24 10:47:20 +0530

    Create 36.py

---
+++ 36.py
@@ -0,0 +1,22 @@
+Input: quantity
+
+cost of a quantity =100
+
+8
+
+purchase_amount=8*100
+
+if(purchase_amount>1000):
+
+#provide 50% discount
+
+Discount=purchase_amount*0.5
+
+Total=purchase_amount-Discount
+
+Print(“Total: “, Total)
+
+else:
+
+Total=purchase_amount
+print("Total:",Total) 
