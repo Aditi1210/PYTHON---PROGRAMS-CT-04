6f471645fe28364e0c1e122fdd4f65a4d6064310
Aditi1210 <81474485+Aditi1210@users.noreply.github.com>
2021-10-24 16:00:28 +0530

    Create 52.py

---
+++ 52.py
@@ -0,0 +1,15 @@
+def even_values_string(str):
+
+  result = "" 
+
+  for i in range(len(str)):
+
+    if i % 2 == 0:
+
+      result = result + str[i]
+
+  return result
+
+print(even_values_string('abcdef'))
+
+print(even_values_string('python'))
