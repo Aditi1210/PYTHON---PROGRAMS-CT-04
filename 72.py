29a542f4d5a987db1526bcc3f9dc5d4d4d7a6c9f
Aditi1210 <81474485+Aditi1210@users.noreply.github.com>
2021-10-29 18:44:55 +0530

    Create 72.py

---
+++ 72.py
@@ -0,0 +1,17 @@
+print("Enter 10 Elements of List: ")
+
+nums = []
+
+for i in range(10):
+
+    nums.insert(i, input())
+
+print("Enter an Element to Insert at End: ")
+
+elem = input()
+
+nums.append(elem)
+
+print("\nThe New List is: ")
+
+print(nums)
