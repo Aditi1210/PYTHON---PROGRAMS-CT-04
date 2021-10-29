0f29ff73f8bf05be7a70023108a8af16ab7f655d
Aditi1210 <81474485+Aditi1210@users.noreply.github.com>
2021-10-29 12:38:41 +0530

    Create 67.py

---
+++ 67.py
@@ -0,0 +1,27 @@
+def getSum(n):
+
+ 
+
+    sum = 0
+
+    while (n != 0):
+
+ 
+
+        sum = sum + int(n % 10)
+
+        n = int(n/10)
+
+ 
+
+    return sum
+
+ 
+
+ 
+
+# Driver code
+
+n = 687
+
+print(getSum(n))
