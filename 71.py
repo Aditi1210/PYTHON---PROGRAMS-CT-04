e3fe8ead47d0fbd50e8f102635fad05134e79577
Aditi1210 <81474485+Aditi1210@users.noreply.github.com>
2021-10-29 18:44:29 +0530

    Create 71.py

---
+++ 71.py
@@ -0,0 +1,9 @@
+number1 = input('Enter first number: ')
+
+number2 = input('Enter second number: ')
+
+number3 = input('Enter third number')
+
+sum = int(number1) + int(number2) + int(number3)
+
+print(sum)
